093d6e3d3447fcfec5673bd5b4e20b9a0d758dab
toiQS <nguyensieu12112002@gmail.com>
2023-04-11 19:47:04 +0700

    Add project files.

---
+++ Web-App Ban Hang/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_App_Ban_Hang.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Web-App Ban Hang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_App_Ban_Hang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
