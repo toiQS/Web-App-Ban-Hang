ba5bbbc7df10c3954863cb9e313becce5137945f
toiQS <nguyensieu12112002@gmail.com>
2023-04-18 12:42:19 +0700

    nocode

---
+++ Presistence/ApplicationDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp_Ban_Hang.Entity;
+
+namespace WebApp_Ban_Hang.Presistence
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+        public DbSet<Account> Account { get; set; }
+        public DbSet<AccountGroup> AccountGroup { get; set; }
+        public DbSet<AccountPermission> AccountPermission { get; set; }
+        public DbSet<AccountType> AccountType { get; set; }
+        public DbSet<Brand> Brand { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductImage> ProductImage { get; set; }
+        public DbSet<ProductInfo> ProductInfo { get; set; }
+        public DbSet<ProductWarranty> ProductWarranty { get; set;}
+        public DbSet<UserDetail> UserDetail { get; set; }
+        public DbSet<UserOrder> UserOrder { get; set; }
+    }
+}
--- Presistence/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Presistence
-{
-    public class Class1
-    {
-
-    }
-}
