3fc5efe66ad4f1df739aa2e21c6c0573508068f2
toiQS <nguyensieu12112002@gmail.com>
2023-04-19 00:08:23 +0700

    edit

---
+++ Web-App Ban Hang/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_App_Ban_Hang.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
